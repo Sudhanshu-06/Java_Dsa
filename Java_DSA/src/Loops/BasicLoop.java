cf34a3736435202d4c7824b9273419e60e2aa3f3
Sudhanshu-06 <sudhanshu767778@gmail.com>
2026-01-18 20:44:34 +0530

    class code done

---
+++ Java_DSA/src/Loops/BasicLoop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class BasicLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=1; i<=10; i++){
+            System.out.print(i+2 + " ");
+        }
+    }
+}
---
+++ Java_DSA/src/Loops/OddNumber1To100.java
@@ -0,0 +1,16 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class OddNumber1To100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:- ");
+        int n= sc.nextInt();
+        for(int i =1; i<=n; i++){
+            if(i%2==1){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
